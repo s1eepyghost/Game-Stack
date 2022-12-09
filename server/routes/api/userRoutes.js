a82bbd79cd19d8dff62ebf475683728b56ca8c7f
Justin Liu <justinwl2097@gmail.com>
2022-12-08 19:58:43 -0600

    added savedPlatforms array to user type and made user routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,98 +1,121 @@
 const router = require('express').Router();
-const User = require('../../models/User');
+// const User = require('../../models/User');
+const {
+  getSingleUser,
+  createUser,
+  login,
+  saveGame,
+  deleteGame,
+  addPlatform,
+  removePlatform,
+} = require('../../controllers/user-controller');
 
-//find all users
-router.get('/', async (req, res) => {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password']}
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-    res.json(userData);
-  });
+const { authMiddleware } = require('../../utils/auth');
 
-//TO DO: find specific user
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      user_id: req.params.id
-    }
-    .then(UserData => {
-      if (!UserData) {
-        res.status(404).json({ message: 'No user with this id' });
-        return;
-      }
-      res.json(UserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    }),
-  })
-});
+router.route('/').post(createUser).put(authMiddleware, saveGame);
 
-//add a user
-//expecting username, first_name, last_name, valid email, password
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create({
-        username: req.body.username,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password
-      })
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+router.route('/login').post(login);
 
-// login
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+router.route('/self').get(authMiddleware, getSingleUser);
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email, please try again' });
-      return;
-    }
+router.route('/platforms').put(authMiddleware, addPlatform);
 
-    const validPassword = await userData.checkPassword(req.body.password);
+router.route('/user/:userId').get(getSingleUser);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect password, please try again' });
-      return;
-    }
+router.route('/games/:gameId').delete(authMiddleware, deleteGame);
 
-    req.session.save(() => {
-      req.session.user_id = userData.user_id;
-      req.session.username = userData.username;
-      req.session.logged_in = true;
+// //find all users
+// router.get('/', async (req, res) => {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password']}
+//     })
+//     .catch((err) => {
+//       res.json(err);
+//     });
+//     res.json(userData);
+//   });
 
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
+// //TO DO: find specific user
+// router.get('/:id', (req, res) => {
+//   User.findOne({
+//     attributes: { exclude: ['password'] },
+//     where: {
+//       user_id: req.params.id
+//     }
+//     .then(UserData => {
+//       if (!UserData) {
+//         res.status(404).json({ message: 'No user with this id' });
+//         return;
+//       }
+//       res.json(UserData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }),
+//   })
+// });
 
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});  
+// //add a user
+// //expecting username, first_name, last_name, valid email, password
+// router.post('/', async (req, res) => {
+//     try {
+//       const userData = await User.create({
+//         username: req.body.username,
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         email: req.body.email,
+//         password: req.body.password
+//       })
+//       res.status(200).json(userData);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
 
-//Logout route
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
+// // login
+// router.post('/login', async (req, res) => {
+//   try {
+//     const userData = await User.findOne({ where: { email: req.body.email } });
+
+//     if (!userData) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email, please try again' });
+//       return;
+//     }
+
+//     const validPassword = await userData.checkPassword(req.body.password);
+
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect password, please try again' });
+//       return;
+//     }
+
+//     req.session.save(() => {
+//       req.session.user_id = userData.user_id;
+//       req.session.username = userData.username;
+//       req.session.logged_in = true;
+
+//       res.json({ user: userData, message: 'You are now logged in!' });
+//     });
+
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });  
+
+// //Logout route
+// router.post('/logout', (req, res) => {
+//   if (req.session.logged_in) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
   module.exports = router;
