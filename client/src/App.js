31631b49430966bacf1426b817ac8c64aac0f2bb
Justin Liu <justinwl2097@gmail.com>
2022-12-12 18:49:49 -0600

    added apollo/client to client package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import { BrowserRouter as Router, Routes, Route, Outlet, Link } from "react-router-dom"
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Home from "./Components/Home"
 import About from "./Components/About"
 import React, { lazy, Suspense } from "react"
 
 // const Home = lazy(() => import("./Components/Home"))
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY
