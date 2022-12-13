c486e646c3fe4f67457d43ab6a0dd39ef1432fa6
Justin Liu <justinwl2097@gmail.com>
2022-12-12 20:18:26 -0600

    added API.js for RAWG API calls

---
+++ client/src/utils/API.js
@@ -0,0 +1,15 @@
+/* Sends a query to the RAWG database to search for games using a search term.
+Todo: should maybe limit how many to search for?
+Todo: Also figure out how to secretly send API keys to these functions
+*/
+export const searchRAWG = (query) => {
+    console.log(`https://api.rawg.io/api/games?key=385e31d0dc754439817581587a08cf36&search=${query}`);
+    return fetch(`https://api.rawg.io/api/games?key=385e31d0dc754439817581587a08cf36&search=${query}`);
+}
+
+/*
+    Sends a search query to RAWG to get any game platforms it recognizes.
+*/
+export const searchRAWGPlatforms = (query) => {
+
+}
