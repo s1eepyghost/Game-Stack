9705ae3b132574638ae30546289e4d2874716a79
Justin Liu <justinwl2097@gmail.com>
2022-12-14 19:55:00 -0600

    started work on games page

--- client/src/Pages/Games.js
+++ client/src/Pages/Games.js
@@ -1,4 +1,18 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+
+import Auth from '../utils/auth';
+import { searchGoogleBooks } from '../utils/API';
+
+import { SAVE_GAME } from '../utils/mutations';
+
+const SearchGames = () => {
+    const [searchedGames, setSearchedBooks] = useState([]);
+
+    const [searchInput, setSearchInput] = useState('');
+
+    const [saveGame, { error }] = useMutation(SAVE_BOOK);
+}
 
 function Games() {
     // Todo: have initial API query of the top games from RAWG? May also need to add a Search form on this page.
