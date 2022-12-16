68a8348ad670ac7588d54bda4c24294a1510518c
Justin Liu <justinwl2097@gmail.com>
2022-12-15 18:18:33 -0600

    saving work before pulling

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -9,6 +9,27 @@ import { DELETE_GAME } from '../utils/mutations';
 function Profile() {
     const { loading, data } = useQuery(GET_SELF);
     const userData = data?.self || {};
+    const [deleteGame, { error }] = useMutation(DELETE_GAME);
+
+    const handleDeleteGame = async (gameId) => {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
+        }
+
+        try {
+            await deleteGame({ variables: {gameId: gameId }});
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+    if (loading) {
+        return <h2>Grabbing data...</h2>;
+    }
+
     return (
         <>
             <h1>Your Game Stack</h1>
