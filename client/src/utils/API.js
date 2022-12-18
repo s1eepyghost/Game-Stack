e0f4672c10684c80952d021699fc35c61d114e5f
Justin Liu <justinwl2097@gmail.com>
2022-12-18 12:19:26 -0600

    hard-coded api key into api calls, this is very insecure and should be changed to something more secure if this is ever returned to!

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,12 +4,12 @@ Todo: Also figure out how to secretly send API keys to these functions
 */
 
 export const searchRAWG = (query) => {
-    return fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&search=${query}`);
+    return fetch(`https://api.rawg.io/api/games?key=385e31d0dc754439817581587a08cf36&search=${query}`);
 }
 
 /*
     Sends a search query to RAWG to get any game platforms it recognizes.
 */
 export const searchRAWGPlatforms = (query) => {
-    return fetch(`https://api.rawg.io/api/platforms?key=${process.env.REACT_APP_API_KEY}&search=${query}`);
+    return fetch(`https://api.rawg.io/api/platforms?key=385e31d0dc754439817581587a08cf36&search=${query}`);
 }
